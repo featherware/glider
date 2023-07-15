564bfa86fe7897cb0cb564654dc0650c4c43d190
Luan van Pletsen <luan@Luans-MacBook-Pro.local>
2023-07-15 22:46:00 +0100

    wtf

---
+++ visualize.py
@@ -0,0 +1,34 @@
+import mujoco
+import mediapy as media
+
+def render_to_collision(model, data, framerate=60, show=True):
+    renderer = mujoco.Renderer(model)
+    frames = []
+    mujoco.mj_resetData(model, data)  # Reset state and time.
+    while len(data.contact) < 1:  # Render until landing
+        mujoco.mj_step(model, data)
+        if len(frames) < data.time * framerate:
+            renderer.update_scene(data)
+            pixels = renderer.render()
+            frames.append(pixels)
+    if show:
+    	media.show_video(frames, fps=framerate)
+        
+    return frames
+    
+
+def render_for_time_secs(model, data, duration=5, framerate=60, show=True):
+    renderer = mujoco.Renderer(model)
+    frames = []
+    mujoco.mj_step(model, data)
+    
+    while data.time < duration:
+        mujoco.mjstep(model, data)
+        if len(frames) < data.time * framerate:
+            renderer.update_scene(data)
+            pixels = renderer.render()
+            frames.append(pixels)
+    if show:
+    	media.show_video(frames, fps=framerate)
+        
+    return frames
