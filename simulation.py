23c80740bd0865f68a67b7128e3b994c243b378e
Luan van Pletsen <luan@Luans-MacBook-Pro.local>
2023-07-15 23:19:18 +0100

    Add drop test

--- simulation.py
+++ simulation.py
@@ -19,3 +19,23 @@ def wrap_glider(glider_xml: str) -> str:
 """
 
     return world_xml
+
+
+def drop_test_glider(glider_xml: str, distance=50) -> str:
+    world_xml = f"""
+<mujoco>
+    <option density="{DENSITY}" viscosity="{VISCOSITY}" wind="{WIND}"/>
+    <worldbody>
+        <light name="top" pos="0 0 5"/>
+        <!-- Body -->
+        {glider_xml}
+        <!-- Landing Platform -->
+        <body name="platform" pos="0 0 0">
+            <geom name="platform-geom" type="box" size="1500 1500 1" rgba="1 1 1 1" pos="0 0 {-distance}"/>
+        </body>
+    </worldbody>
+</mujoco>
+
+"""
+    print(world_xml)
+    return world_xml
