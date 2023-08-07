7b3658ef3920875e7ae4d56be57ae9a8c2655ae9
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-08-07 09:18:44 +0100

    Extract observability module

--- optimization.py
+++ optimization.py
@@ -1,51 +1,44 @@
 import mujoco
-import numpy as np
 import pandas as pd
 
-import visualize
-from constants import DEFAULT_STL_FILEPATH, GLIDER_GEOM_NAME
+from constants import DEFAULT_STL_FILEPATH
+from observability import glider_abs_x_position
 from simulation import drop_test_glider
 from vehicle import create_glider_xml
 
 
 def find_best_orientation(
-        stl_filename: str = DEFAULT_STL_FILEPATH,
-        granularity_deg: int = 50,
-        max_results: int = 10,
-        ) -> list[int]:
-
+    stl_filename: str = DEFAULT_STL_FILEPATH,
+    granularity_deg: int = 50,
+    max_results: int = 10,
+) -> list[int]:
     # array = [
-    #     pd.DataFrame( 
+    #     pd.DataFrame(
     #         [[x, y, z, distance_score(x,y,z)]],
     #         columns=['x', 'y', 'z', 'distance'],
-    #         ) 
+    #         )
     #     for x in range(0,360, granularity)
     #     for y in range(0,360, granularity)
     #     for z in range(0, 360, granularity)
     # ]
 
-    df = pd.DataFrame(columns=['x', 'y', 'z', 'distance'])
+    df = pd.DataFrame(columns=["x", "y", "z", "distance"])
     df = pd.concat(
         [
             pd.DataFrame(
-                [[
-                    x,
-                    y,
-                    z,
-                    measure_drop_test(orientation=[x, y, z])
-                    ]],
-                columns=['x', 'y', 'z', 'distance'],
-                ) 
-            for x in range(0,360, granularity_deg)
-            for y in range(0,360, granularity_deg)
+                [[x, y, z, measure_drop_test(orientation=[x, y, z])]],
+                columns=["x", "y", "z", "distance"],
+            )
+            for x in range(0, 360, granularity_deg)
+            for y in range(0, 360, granularity_deg)
             for z in range(0, 360, granularity_deg)
         ],
         ignore_index=True,
     )
 
-    print("Furthest distance: ", df['distance'].max())
-    print("Best orientation", df[df['distance'] == df['distance'].max()])
-    return df[df['distance'] == df['distance'].max()]
+    print("Furthest distance: ", df["distance"].max())
+    print("Best orientation", df[df["distance"] == df["distance"].max()])
+    return df[df["distance"] == df["distance"].max()]
 
 
 def measure_drop_test(
@@ -56,9 +49,7 @@ def measure_drop_test(
     wind: str = "0 0 0",
 ) -> float:
     glider_xml, glider_asset = create_glider_xml(
-        filename=stl_filename,
-        orientation=orientation,
-        scale=scale
+        filename=stl_filename, orientation=orientation, scale=scale
     )
     assert glider_xml
     assert glider_asset
@@ -72,4 +63,4 @@ def measure_drop_test(
     while len(data.contact) < 1:  # Render until landing
         mujoco.mj_step(model, data)
 
-    return abs(data.geom(GLIDER_GEOM_NAME).xpos[0])
+    return glider_abs_x_position(data)
