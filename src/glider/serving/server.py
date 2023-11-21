5ef186e20cfc1c274fbc0d081a0d42cf73019482
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-11-21 09:21:21 +0000

    Add vehicle test route

--- src/glider/serving/server.py
+++ src/glider/serving/server.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+import glider
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/vehicle")
+def read_vehicle():
+    return bool(glider)
