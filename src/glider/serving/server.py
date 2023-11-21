9790bd90053aee090d072602c356bef1c6fbaf75
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-11-21 12:57:19 +0000

    Add /vehicle/fitness/ route

--- src/glider/serving/server.py
+++ src/glider/serving/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from schema import VehicleType
 
-from glider import simulation, vehicle
+from glider import optimization, simulation, vehicle
 
 app = FastAPI()
 
@@ -12,7 +12,10 @@ def read_root():
 
 
 @app.post("/vehicle/")
-async def create_vehicle(vertices: list[list[int]], faces: list[list[int]]):
+async def create_vehicle(
+    vertices: list[list[int]] | None = None,
+    faces: list[list[int]] | None = None,
+):
     return vehicle.Vehicle(vertices=vertices, faces=faces)
 
 
@@ -20,3 +23,9 @@ async def create_vehicle(vertices: list[list[int]], faces: list[list[int]]):
 async def drop_test_vehicle(v: VehicleType):
     test_vehicle = vehicle.Vehicle(**v.dict())
     return simulation.drop_test_glider(test_vehicle)
+
+
+@app.post("/vehicle/fitness/")
+async def vehicle_fitness(v: VehicleType) -> float:
+    test_vehicle = vehicle.Vehicle(**v.dict())
+    return optimization.fitness_func(test_vehicle)
