8c8e4c38a934fc1d88bad09c4870767c70f66f71
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-11-23 21:09:06 +0100

    Add frontend

--- backend/src/glider/serving/server.py
+++ backend/src/glider/serving/server.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from schema import VehicleType
+from Pillow import Image
 
-from glider import optimization, simulation, vehicle
+from glider import optimization, simulation, vehicle, visualize
 
 app = FastAPI()
 
@@ -23,6 +24,12 @@ async def create_vehicle(
 async def drop_test_vehicle(v: VehicleType):
     test_vehicle = vehicle.Vehicle(**v.dict())
     return simulation.drop_test_glider(test_vehicle)
+
+
+@app.post("/vehicle/view/")
+async def view_vehicle(v: VehicleType):
+    test_vehicle = vehicle.Vehicle(**v.dict())
+    return {"image": Image(visualize.view_vehicle(test_vehicle)).to_base64()}
 
 
 @app.post("/vehicle/fitness/")
