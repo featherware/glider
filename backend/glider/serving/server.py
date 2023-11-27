ea1f029edd1dfb8df84ff4ee4bc9d7cd8155df33
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-11-27 12:18:49 +0100

    Serve /vehicle/view b64 images

--- backend/glider/serving/server.py
+++ backend/glider/serving/server.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from PIL import Image
+from base64 import b64encode
+from io import BytesIO
 
 from .schema import VehicleType
 
@@ -26,10 +29,13 @@ async def drop_test_vehicle(v: VehicleType):
     return simulation.drop_test_glider(test_vehicle)
 
 
-# @app.post("/vehicle/view/")
-# async def view_vehicle(v: VehicleType):
-#     test_vehicle = vehicle.Vehicle(**v.dict())
-#     return {"image": Image(visualize.view_vehicle(test_vehicle)).to_base64()}
+@app.post("/vehicle/view/")
+async def view_vehicle(v: VehicleType):
+    test_vehicle = vehicle.Vehicle(**v.model_dump())
+    bytes = BytesIO()
+    Image.fromarray(visualize.view_vehicle(test_vehicle)).save(bytes, format="PNG")
+    b64_image = b64encode(bytes.getvalue())
+    return {"image": b64_image.decode("utf-8")}
 
 
 @app.post("/vehicle/fitness/")
